f92130e1b11ca74a4f0f7d70dbf5280496094354
Do Minh Khanh <dominhkhanh99@gmail.com>
2021-04-24 23:31:41 +0700

    update ( click button go to page => transcript , calendar ), update table transcript.

---
+++ lib/src/resources/calendar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';  
+
+class calendar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Calendar",
+          ),
+        ),
+    );
+  }
+}
